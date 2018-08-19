72abd3d132ad47bd25599aa2c85c4801cca5f64b
ladyada <limor@ladyada.net>
2018-08-18 21:14:29 -0400

    reasonable formatting on lat/long, convert all strings to bytes, add pyserial example

--- examples/gps_simpletest.py
+++ examples/gps_simpletest.py
@@ -18,8 +18,12 @@ TX = board.TX
 # a slightly higher timeout (GPS modules typically update once a second).
 uart = busio.UART(TX, RX, baudrate=9600, timeout=3000)
 
+# for a computer, use the pyserial library for uart access
+#import serial
+#uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=3000)
+
 # Create a GPS module instance.
-gps = adafruit_gps.GPS(uart)
+gps = adafruit_gps.GPS(uart, debug=False)
 
 # Initialize the GPS module by changing what data it sends and at what rate.
 # These are NMEA extensions for PMTK_314_SET_NMEA_OUTPUT and
@@ -28,22 +32,22 @@ gps = adafruit_gps.GPS(uart)
 #   https://cdn-shop.adafruit.com/datasheets/PMTK_A11.pdf
 
 # Turn on the basic GGA and RMC info (what you typically want)
-gps.send_command('PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
 # Turn on just minimum info (RMC only, location):
-#gps.send_command('PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+#gps.send_command(b'PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
 # Turn off everything:
-#gps.send_command('PMTK314,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+#gps.send_command(b'PMTK314,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
 # Tuen on everything (not all of it is parsed!)
-#gps.send_command('PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0')
+#gps.send_command(b'PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0')
 
 # Set update rate to once a second (1hz) which is what you typically want.
-gps.send_command('PMTK220,1000')
+gps.send_command(b'PMTK220,1000')
 # Or decrease to once every two seconds by doubling the millisecond value.
 # Be sure to also increase your UART timeout above!
-#gps.send_command('PMTK220,2000')
+#gps.send_command(b'PMTK220,2000')
 # You can also speed up the rate, but don't go too fast or else you can lose
 # data during parsing.  This would be twice a second (2hz, 500ms delay):
-#gps.send_command('PMTK220,500')
+#gps.send_command(b'PMTK220,500')
 
 # Main loop runs forever printing the location, etc. every second.
 last_print = time.monotonic()
@@ -71,8 +75,8 @@ while True:
             gps.timestamp_utc.tm_hour,  # not get all data like year, day,
             gps.timestamp_utc.tm_min,   # month!
             gps.timestamp_utc.tm_sec))
-        print('Latitude: {} degrees'.format(gps.latitude))
-        print('Longitude: {} degrees'.format(gps.longitude))
+        print('Latitude: {0:.6f} degrees'.format(gps.latitude))
+        print('Longitude: {0:.6f} degrees'.format(gps.longitude))
         print('Fix quality: {}'.format(gps.fix_quality))
         # Some attributes beyond latitude, longitude and timestamp are optional
         # and might not be present.  Check if they're None before trying to use!
