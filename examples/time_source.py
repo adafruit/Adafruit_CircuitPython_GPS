8176e15e50a05eafd5173eecd86b6c5b5bf316cc
Pierrick C <arofarn@arofarn.info>
2018-08-24 23:25:17 +0200

    Updated example after pylint checks *No space allowed around keyword argument assignment *Multiple import + comments

--- examples/time_source.py
+++ examples/time_source.py
@@ -1,7 +1,14 @@
-import busio, board, time, rtc
+# Simple script using GPS timestamps as RTC time source
+# The GPS timestamps are available without a fix and keep the track of
+# time while there is powersource (ie coin cell battery)
+
+import time
+import board
+import rtc
+import busio
 import adafruit_gps
 
-uart = busio.UART(board.TX, board.RX, baudrate = 9600, timeout=3000)
+uart = busio.UART(board.TX, board.RX, baudrate=9600, timeout=3000)
 
 gps = adafruit_gps.GPS(uart, debug=False)
 gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
@@ -18,6 +25,7 @@ while True:
     current = time.monotonic()
     if current - last_print >= 1.0:
         last_print = current
+        # Time & date from GPS informations
         print('Fix timestamp: {:02}/{:02}/{} {:02}:{:02}:{:02}'.format(
             gps.timestamp_utc.tm_mon,   # Grab parts of the time from the
             gps.timestamp_utc.tm_mday,  # struct_time object that holds
@@ -25,6 +33,8 @@ while True:
             gps.timestamp_utc.tm_hour,  # not get all data like year, day,
             gps.timestamp_utc.tm_min,   # month!
             gps.timestamp_utc.tm_sec))
+
+        #Time & date from internal RTC
         print('RTC timestamp: {:02}/{:02}/{} {:02}:{:02}:{:02}'.format(
             rtc.RTC.datetime.tm_mon,
             rtc.RTC.datetime.tm_mday,
@@ -32,6 +42,8 @@ while True:
             rtc.RTC.datetime.tm_hour,
             rtc.RTC.datetime.tm_min,
             rtc.RTC.datetime.tm_sec))
+
+        #Time & date from time.localtime() function
         local_time = time.localtime()
         print("Local time: {:02}/{:02}/{} {:02}:{:02}:{:02}".format(
             local_time.tm_mon,
