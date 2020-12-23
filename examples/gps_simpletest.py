6b43dbc41e3411ab8163acc721a19e397b3ff13c
David Dyck <david.dyck@gmail.com>
2020-12-23 05:34:24 -0800

    Fix typo s/# Tuen on/# Turn on/ in examples/gps_simpletest.py

--- examples/gps_simpletest.py
+++ examples/gps_simpletest.py
@@ -36,7 +36,7 @@ gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
 # gps.send_command(b'PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
 # Turn off everything:
 # gps.send_command(b'PMTK314,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
-# Tuen on everything (not all of it is parsed!)
+# Turn on everything (not all of it is parsed!)
 # gps.send_command(b'PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0')
 
 # Set update rate to once a second (1hz) which is what you typically want.
